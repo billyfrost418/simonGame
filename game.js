de698685c9afe70565a37ec0d65ec320ffe35d20
Khánh <khanhlb418@gmail.com>
2019-06-07 23:31:53 +0700

    add more animation

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var started = false;
 var level = 0;
+var score = 0;
 // for Game pattern
 var gamePattern = [];
 // for Users'Pattern
@@ -36,14 +37,13 @@ $(".start-btn").click(function() {
   }
 });
 
-
-
 // check answer every time clicking buttons
 function checkAnswer(currentLevel) {
   // answer is right
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
     // check until the last element in the array
     if (userClickedPattern.length === gamePattern.length) {
+      score++;
       setTimeout(function() {
         nextSequence();
       }, 1000);
@@ -51,19 +51,25 @@ function checkAnswer(currentLevel) {
   } else { // wrong answer
     playSound("wrong");
     $("body").addClass("game-over");
-    $("#level-title").text("Game Over, Your score is: "+ level +"\nPress Any Key to Restart");
+    $("#level-title").text("Game Over");
+    $(".below-title").text("Oops!");
     setTimeout(function() {
       $("body").removeClass("game-over");
     }, 200);
+    setTimeout(function() {
+      $("#level-title").text("Press Any Key to Restart");
+      $(".below-title").text("Let's try again");
+    }, 1200);
+
 
     startOver(); // start the game again
   }
 }
 
-
 function nextSequence() {
   level++;
-  $("#level-title").text("Level " + level);
+  encourage(score);
+  $("#level-title").text("Score " + score);
   userClickedPattern = [];
 
   var randomNumber = Math.floor(Math.random() * 4);
@@ -81,7 +87,6 @@ function playSound(name) {
   audio.play();
 }
 
-
 function animatePress(currentColour) {
   $("#" + currentColour).addClass("pressed");
   setTimeout(function() {
@@ -92,6 +97,48 @@ function animatePress(currentColour) {
 // restore to initial state
 function startOver() {
   level = 0;
+  score = 0;
   gamePattern = [];
   started = false;
+}
+
+// add animation
+function encourage(score) {
+  switch (score) {
+    case 0:
+      $(".below-title").text("Tab the blink button!");
+      break;
+    case 1:
+      $(".below-title").text("Well done");
+      break;
+    case 2:
+      $(".below-title").text("You're doing great!");
+      break;
+    case 3:
+      $(".below-title").text("Excellent!");
+      break;
+    case 4:
+      $(".below-title").text("Unbelievable!");
+      break;
+    case 5:
+      $(".below-title").text("Your memory is a treasure!");
+      break;
+    case 6:
+      $(".below-title").text("Keep playing");
+      break;
+    case 7:
+      $(".below-title").text("Nothing can stop you");
+      break;
+    case 8:
+      $(".below-title").text("❤");
+      break;
+    case 9:
+      $(".below-title").text("❤❤❤");
+      break;
+    case 10:
+      $(".below-title").text("You own my heart!");
+      break;
+    default: $(".below-title").text("😁");
+
+  }
 }
